05640fe96f8bb207b0866c818545d9c0a932523d
giwl-21 <muhsin.wahianwar@gmail.com>
2021-12-11 02:00:37 -0500

    Added SampleExperience.vue for workflow purposes

---
+++ src/pages/SampleExperience.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+  <div style="white-space:pre-line;">
+    <h2><!---Title---></h2>
+    <i><!---February - April 2021--></i>
+<p>
+<!---Intro Paragraphs--->
+</p>
+    <h3>Tools <a class="btn btn-sm btn-link" data-toggle="collapse" data-target="#ov-tools">SHOW</a></h3>
+    <div class="collapse in show" id="ov-tools" style="line-height: 1em">
+    <ul>
+      <!--<li><p><b>Tool name</b></p></li>-->
+    </ul>    
+    </div>
+<p>
+<!---Essay Paragraph 1--->
+
+<!---Essay Paragraph 2--->
+</p>
+
+    </div>
+  </div>
+</template>
